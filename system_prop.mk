fd8dd1690e136c5d1c2ef29b9a8e068feddae478
Adrian DC <radian.dc@gmail.com>
2017-07-31 23:49:09 +0200

    g3: Disable EGL buffer_age extension support
     * Due to commit I9b346b4053ec12c8a78a143a4dc0e708c44888a2
        "Support EGL_KHR_partial_update without EGL_EXT_buffer_age"
        in libhwui, EGL_EXT_buffer_age extension was forcibly enabled
        if EGL_KHR_partial_update was available, even if the EGL driver
        was not providing EGL_EXT_buffer_age support
    
     * On older Adreno drivers, renderthread sources could use bufferAge
        specific swap behaviours and would result in partially missing
        display redraws upon animations and rotated screens,
        for instance flashing action bars, blinking loading bars
        and half black displays in landscape rotations
    
    Change-Id: I16fe13f0726792522e382716f8f24eccf2a27701
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -30,6 +30,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.hwui.use_buffer_age=false \
     persist.hwc.mdpcomp.enable=true \
     ro.opengles.version=196608 \
     ro.sf.lcd_density=480
