9cbb482727b9184f04d83afc22264d4e6f6a743f
Kshitij Gupta <kshitijgm@gmail.com>
2017-10-31 17:23:37 +0100

    g2:Add Vibrator HIDL packages

--- g2.mk
+++ g2.mk
@@ -235,6 +235,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/touch_dev.idc:system/usr/idc/touch_dev.idc
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
+
 # Wifi
 PRODUCT_PACKAGES += \
     libwpa_client \
