e41ead27cbb6329e5b053fc74c16207848bd401c
nasty007 <lazic.josip@gmail.com>
2017-10-31 18:04:19 +0100

    g2: Add Camera HIDL

--- g2.mk
+++ g2.mk
@@ -103,7 +103,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@3.2-impl \
     camera.msm8974
 
 # Charger
